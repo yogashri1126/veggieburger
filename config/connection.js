27e8ca83d21aa0b5e53b949a5c1fde787564c965
yogashri1126 <ypradhan.pge@texas.edu>
2017-10-08 00:06:37 -0500

    adding the environment variable

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + "/config.json")[env];
+var connection;
+
+//console.log(config);
+
+//determine env 
+if(config.use_env_variable){
+  var connection = mysql.createConnection(process.env[config.use_env_variable]);
+}else{
+
+connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "",
-  database: "cat_db"
+  password: "MyNewPass",
+  database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -20,3 +32,4 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
