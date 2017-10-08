1d63c06f124bc5c54e449c2d2aae4eb99f19e225
yogashri1126 <ypradhan.pge@texas.edu>
2017-10-08 01:46:21 -0500

    added env variable to server.js

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date_added TIMESTAMP
+	date_added TIMESTAMP,
 	PRIMARY KEY (id)
 );
