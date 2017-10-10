e83acf728e8589e895a599c0fd25fa14f785d544
yogashri1126 <ypradhan.pge@texas.edu>
2017-10-09 19:10:00 -0500

    I'm responding to heroku logs in that I fixed my server.js

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,53 @@
 // Set up MySQL connection.
+// var mysql = require("mysql");
+
+// var env = process.env.NODE_ENV || 'development';
+// var config = require(__dirname + "/config.json")[env];
+// var connection;
+
+// //console.log(config);
+
+// //determine env 
+// if(config.use_env_variable){
+//   var connection = mysql.createConnection(process.env[config.use_env_variable]);
+// }else{
+
+// connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "MyNewPass",
+//   database: "burgers_db"
+// });
+// }
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
 var mysql = require("mysql");
+
 
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + "/config.json")[env];
-var connection;
 
-//console.log(config);
+console.log(config);
 
-//determine env 
 if(config.use_env_variable){
   var connection = mysql.createConnection(process.env[config.use_env_variable]);
 }else{
-
-connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "MyNewPass",
-  database: "burgers_db"
-});
+  var connection = mysql.createConnection(config);
 }
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -30,6 +56,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
 
